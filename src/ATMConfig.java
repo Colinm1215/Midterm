180ac6a4e8e20a69ce2516b1771d4bb36c2bd565
Colinm1215 <33791959+Colinm1215@users.noreply.github.com>
2025-03-18 15:46:57 -0400

    ATMConfig

--- src/ATMConfig.java
+++ src/ATMConfig.java
@@ -4,5 +4,40 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class ATMConfig implements ConfigInterface {
+    private static final String CONFIG_FILE = "config.properties";
+    private static final Properties props = new Properties();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    static {
+        try (InputStream input = ATMConfig.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
+            if (input == null) {
+                System.err.println("Unable to find " + CONFIG_FILE);
+            } else {
+                props.load(input);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    @Override
+    public String getDB_URL() {
+        return props.getProperty("DB_URL");
+    }
+
+    @Override
+    public String getDB_USER() {
+        return props.getProperty("DB_USER");
+    }
+
+    @Override
+    public String getDB_PASSWORD() {
+        return props.getProperty("DB_PASSWORD");
+    }
 
 }
